90fd22328e156c477f7f2a86dda2de32a0ede472
hadje <hadjera.emroska@student.vinci.be>
2023-10-02 15:25:22 +0200

    ex 1.1 et 1.2 +tutorials

--- web2/exercices/1.1/app.js
+++ web2/exercices/1.1/app.js
@@ -3,9 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-var usersRouter = require('./routes/users');
-var pizzaRouter = require('./routes/pizzas');
+var filmsRouter = require('./routes/films');
 
 
 var app = express();
@@ -14,9 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/users', usersRouter);
-app.use('/pizzas', pizzaRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
